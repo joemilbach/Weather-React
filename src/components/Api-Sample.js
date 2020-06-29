c9ba7f9460f78a23614ae4fa5c0b0b0c152a5868
Joe Milbach <jmilbach@cws.net>
2020-06-29 16:43:09 -0500

    Add sample API component

---
+++ src/components/Api-Sample.js
@@ -0,0 +1,8 @@
+const Api = {
+  weatherKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  weatherBaseUrl: "http://api.openweathermap.org/data/2.5/",
+  mapKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  mapBaseUrl: "http://open.mapquestapi.com/geocoding/v1/"
+}
+
+export default Api;
