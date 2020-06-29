5348dcdbdfb46ed0e82a8f17732c0b10aa8a061b
Joe Milbach <jmilbach@cws.net>
2020-06-29 16:41:34 -0500

    Add to .gitignore

--- src/components/api.js
+++
@@ -1,8 +0,0 @@
-const Api = {
-  weatherKey: "faefd155e08c7c2328051dba4988037a",
-  weatherBaseUrl: "http://api.openweathermap.org/data/2.5/",
-  mapKey: "UvcuDMEcFhYpPlaDyTcm8ZHBbQTXAAOt",
-  mapBaseUrl: "http://open.mapquestapi.com/geocoding/v1/"
-}
-
-export default Api;
